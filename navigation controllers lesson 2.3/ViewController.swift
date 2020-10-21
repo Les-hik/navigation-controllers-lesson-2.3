7e2773bbd24fb23cc66622ea34179f3060f9e756
Alex <ale-x@list.ru>
2020-10-21 17:17:41 +0300

    Initial Commit

---
+++ navigation controllers lesson 2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigation controllers lesson 2.3
+//
+//  Created by Alex on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
